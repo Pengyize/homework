bd2cb468832009f0afeb18fcb425f0d90ca5b688
pengyize <649786395@qq.com>
2018-03-25 11:08:09 +0800

    更新简历

--- resume/main.js
+++ resume/main.js
@@ -13,29 +13,45 @@ window.onscroll = function (xxx) {
 };
 
 let liTags = document.querySelectorAll('nav.menu > ul > li');
-for(let i=0;i<  liTags.length;i++){            //for循环，这样就能同时监听数组里的所有值
+for(let i=0;i<  liTags.length;i++){                 //for循环，这样就能同时监听数组里的所有值
     liTags[i].onmouseenter = function (x) {
         // console.log(x.target);                   //x.target是我们操作的那个元素，就是我们点击的元素，若a里包含了个span，那操作的就是span
         // x.currentTarget.classList.remove('leave');
-        x.currentTarget.classList.add('active');    //x.currentTarget是监听的元素，就是classname为menuTrigger的元素
+        x.currentTarget.classList.add('active');    //x.currentTarget是监听的元素，就是li元素
     };
     liTags[i].onmouseleave = function (x) {
         x.currentTarget.classList.remove('active');
         // x.currentTarget.classList.add('leave');
-        // setTimeout(function () {
-        //     x.currentTarget.classList.remove('leave');
-        // }, 1000);
     };
 }
 
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a');
-    for(let i=0;i<aTags.length;i++) {
-        aTags[i].onclick = function (x) {
-            x.preventDefault();
-            let a = x.currentTarget;
-            let href = a.getAttribute('href');
-            let element = document.querySelector(href);        //'#siteAbout'
-            let top = element.offsetTop;
-            window.scrollTo(0,top-120);
-        }
+let aTags = document.querySelectorAll('nav.menu > ul > li > a');
+for(let i=0;i<aTags.length;i++) {
+    aTags[i].onclick = function (x) {
+        x.preventDefault();
+        let a = x.currentTarget;
+        let href = a.getAttribute('href');
+        let element = document.querySelector(href);        //'#siteAbout'
+        let top = element.offsetTop;
+
+        let n = 25; //动多少次
+        let duration = 500 / n; //每次持续的时间
+        let currentTop = window.scrollY;  //当前位置
+        let targetTop = top-120;    //目标位置
+        let distance = (targetTop - currentTop) / n;    //每次动的距离
+        console.log(currentTop);
+
+
+        console.log(distance);
+        let i = 0;
+        let moveToAnchor = setInterval(() => {
+            if(i === 25){
+                window.clearInterval(moveToAnchor);
+                return;
+            }
+            i++;
+            window.scrollTo(0,currentTop + distance * i); //两个参数分别是x和y，浏览器横坐标的值，纵坐标的值
+        },duration);
+
     }
+}
