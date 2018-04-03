1a7a8b3ee4dcab6cbdb6b2ba58cedaf4365a593f
pengyize <649786395@qq.com>
2018-04-03 23:22:05 +0800

    第二个版本

--- playSlide/js/main.js
+++ playSlide/js/main.js
@@ -1,19 +1,67 @@
-setTimeout(function () {
-    $('.images > img:nth-child(1)').css({
-        transform:'translateX(-100%)'
-    });
-    $('.images > img:nth-child(2)').css({
-        transform:'translateX(-100%)'
-    });
-    $('.images > img:nth-child(1)').on('transitionend',function (e) {
-        $(e.currentTarget).css({transform:'none'}).addClass('right');
-    });
-},1000);
-setTimeout(function () {
-    $('.images > img:nth-child(2)').css({
-        transform:'translateX(-200%)'
-    });
-    $('.images > img:nth-child(3)').css({
-        transform:'translateX(-100%)'
-    });
-},2800);
+let n;
+Init();
+
+setInterval(() => {
+    makeLeave(getImage(n))
+        .one('transitionend',(e) => {
+            makeEnter($(e.currentTarget));
+        });
+    makeCurrent(getImage(n+1));
+    n++;
+},2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function x(n) {
+    if(n>3){
+        n = n%3;
+        if(n===0){
+            n=3;
+        }
+    }
+
+    return n;
+}
+
+function Init() {
+    n=1;
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter');
+
+}
+
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave');
+
+}
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter');
+}
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current');
+}
+
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`);
+}
