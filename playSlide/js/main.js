7d9f001e969cbd1a27e843fa8114b8a5833807d2
pengyize <649786395@qq.com>
2018-04-08 21:43:08 +0800

    更新轮播

--- playSlide/js/main.js
+++ playSlide/js/main.js
@@ -1,16 +1,14 @@
-let n;
-Init();
+let $buttons = $('#buttonWrapper>button');
+let $slides = $('#slides');
+let $images = $slides.children('img');
+let $next = $('#next');
+let $previous = $('#previous');
 
-setInterval(() => {
-    makeLeave(getImage(n))
-        .one('transitionend',(e) => {
-            makeEnter($(e.currentTarget));
-        });
-    makeCurrent(getImage(n+1));
-    n++;
-},2000);
+makeFakeSlide();
 
+$slides.css({transform:'translateX(-400px)'});
 
+playSlide();
 
 
 
@@ -33,35 +31,72 @@ setInterval(() => {
 
 
 
-function x(n) {
-    if(n>3){
-        n = n%3;
-        if(n===0){
-            n=3;
-        }
+
+
+
+function makeFakeSlide() {
+    let $firstCopy = $images.eq(0).clone(true);
+    let $lastCopy = $images.eq($images.length-1).clone(true);
+
+    $slides.append($firstCopy);
+    $slides.prepend($lastCopy);
+}
+
+
+let current = 0;
+function playSlide() {
+
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget);
+        let index = $button.index();
+        goToSlide(index);
+
+    });
+}
+
+$next.on('click',function () {
+    goToSlide(current+1);
+});
+$previous.on('click',function () {
+    goToSlide(current-1);
+});
+
+let timer = setInterval(function () {
+    goToSlide((current+1))
+},1700);
+$('.container').on('mouseenter',function () {
+    window.clearInterval(timer);
+}).on('mouseleave',function () {
+    timer = setInterval(function () {
+        goToSlide((current+1))
+    },1700);
+});
+
+function goToSlide(index) {
+    if(index>$buttons.length-1){
+        index=0;
+    }else if(index<0){
+        index = $buttons.length-1
     }
 
-    return n;
-}
-
-function Init() {
-    n=1;
-    $(`.images > img:nth-child(${n})`).addClass('current')
-        .siblings().addClass('enter');
-
-}
-
-function makeLeave($node) {
-    return $node.removeClass('current enter').addClass('leave');
-
-}
-function makeEnter($node) {
-    return $node.removeClass('leave current').addClass('enter');
-}
-function makeCurrent($node) {
-    return $node.removeClass('enter leave').addClass('current');
-}
-
-function getImage(n) {
-    return $(`.images > img:nth-child(${x(n)})`);
+    if(index === 0 && current === $buttons.length-1){
+        $slides.css({transform:`translateX(${-($buttons.length+1)*400}px)`})
+            .one('transitionend',function () {
+                $slides.hide().offset();
+                $slides.css({transform: `translateX(${-(index+1)*400}px)`})
+                    .show();
+            });
+        current = index;
+    }else if(current === 0 && index === $buttons.length-1){
+        $slides.css({transform:`translateX(0px)`})
+            .one('transitionend',function () {
+                $slides.hide().offset();
+                $slides.css({transform: `translateX(${-(index+1)*400}px)`})
+                    .show();
+            });
+        current = index;
+    }else{
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`});
+        current = index;
+    }
 }
