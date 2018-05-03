45456c41aefa2e739698aeeb29a3000605e7b66b
pengyize <649786395@qq.com>
2018-05-03 11:50:17 +0800

    更新简历

--- server-JSONP/server.js
+++ server-JSONP/server.js
@@ -19,14 +19,9 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-
-
-
-
-
-
+  
   if(path === '/'){
-	var string = fs.readFileSync('./sign_up.html', 'utf8')
+	var string = fs.readFileSync('./index.html', 'utf8')
 	var amount = fs.readFileSync('./db','utf8')
 	string = string.replace('&&&amount&&&',amount)
 	response.statusCode = 200
@@ -41,7 +36,7 @@ var server = http.createServer(function(request, response){
 	response.statusCode = 200
 	response.write(`
 		${query.callback}.call(undefined,'success')
-	`)	
+	`)
 	response.end() 
   }else{
    	response.statusCode = 404
@@ -51,16 +46,8 @@ var server = http.createServer(function(request, response){
   }
 
 
-
-
-
-
-
-
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
